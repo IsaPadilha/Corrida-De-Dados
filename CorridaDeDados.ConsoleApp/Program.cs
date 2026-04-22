f886b3f4af9e182fed139585003332f09e23d568
Isadora Padilha <isap4619@gmail.com>
2026-04-22 19:18:14 -0300

    class program feito

--- CorridaDeDados.ConsoleApp/Program.cs
+++ CorridaDeDados.ConsoleApp/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿namespace CorridaDeDados.ConsoleApp;
+
+using CorridaDeDados.ConsoleApp.Entidades;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            jogador.posicao = 0;
+            computador.posicao = 0;
+
+            while (true)
+            {
+                jogador.ExecutarRodada();
+
+                if (jogador.VenceuPartida())
+                    break;
+
+                computador.ExecutarRodada();
+
+                if (computador.VenceuPartida())
+                    break;
+            }
+
+            Console.WriteLine("-------------------------------");
+            Console.Write("Deseja continuar? (s/n)");
+            string? opcaoContinuar = Console.ReadLine()?.ToUpper();
+
+            if (opcaoContinuar != "S")
+                break;
+        }
+    }
+}
